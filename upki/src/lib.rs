12a18943329e2893275e62e53bd0e999cd29074c
Joe Birr-Pixton <jpixton@gmail.com>
2025-12-06 18:20:29 +0000

    Split out common types into `upki` crate

---
+++ upki/src/lib.rs
@@ -0,0 +1,31 @@
+use serde::Serialize;
+
+/// The structure contained in a manifest.json
+#[derive(Clone, Debug, Serialize)]
+pub struct Manifest {
+    /// When this file was generated.
+    ///
+    /// UNIX timestamp in seconds.
+    pub generated_at: u64,
+
+    /// Some human-readable text.
+    pub comment: String,
+
+    /// List of filter files.
+    pub filters: Vec<Filter>,
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct Filter {
+    /// Relative filename.
+    ///
+    /// This is also the suggested local filename.
+    pub filename: String,
+
+    /// File size, indicative.  Allows a fetcher to predict data usage.
+    pub size: usize,
+
+    /// SHA256 hash of file contents.
+    #[serde(with = "hex::serde")]
+    pub hash: Vec<u8>,
+}
