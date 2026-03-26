795bbb7b7bacc3ad330ba78763007d7691721ca8
Joe Birr-Pixton <jpixton@gmail.com>
2026-03-26 20:46:55 +0000

    Fetch intermediates if configured

--- upki/src/lib.rs
+++ upki/src/lib.rs
@@ -61,6 +61,10 @@ impl Config {
     pub(crate) fn revocation_cache_dir(&self) -> PathBuf {
         self.cache_dir.join("revocation")
     }
+
+    fn intermediates_cache_dir(&self) -> PathBuf {
+        self.cache_dir.join("intermediates")
+    }
 }
 
 /// How the path to a configuration file was decided upon.
