43acafd941c175019b3f2ff68f57e700d8060b03
Joe Birr-Pixton <jpixton@gmail.com>
2026-03-26 20:46:55 +0000

    Fetch intermediates if configured

--- upki/src/intermediates.rs
+++ upki/src/intermediates.rs
@@ -1,4 +1,33 @@
+use std::process::ExitCode;
+
 use serde::{Deserialize, Serialize};
+
+use crate::Config;
+use crate::data::{MANIFEST_JSON, Manifest, fetch_inner};
+use crate::revocation::Error;
+
+/// Update the local intermediates cache by fetching updates over the network.
+///
+/// `dry_run` means this call fetches the new manifest, but does not fetch any
+/// required files; but the necessary files are printed to stdout.  Therefore
+/// such a call is not completely "dry" -- perhaps "moist".
+pub async fn fetch(dry_run: bool, config: &Config) -> Result<ExitCode, Error> {
+    let Some(intermediates) = &config.intermediates else {
+        return Ok(ExitCode::SUCCESS);
+    };
+    let mut manifest_path = config.intermediates_cache_dir();
+    manifest_path.push(MANIFEST_JSON);
+    let old_manifest = Manifest::from_file(&manifest_path).ok();
+    let manifest_url = format!("{}{MANIFEST_JSON}", intermediates.fetch_url);
+    fetch_inner(
+        dry_run,
+        &intermediates.fetch_url,
+        manifest_url,
+        &old_manifest,
+        config.intermediates_cache_dir(),
+    )
+    .await
+}
 
 /// Details about intermediate preloading.
 #[derive(Debug, Deserialize, Serialize)]
@@ -14,7 +43,7 @@ impl Default for IntermediatesConfig {
     fn default() -> Self {
         Self {
             enabled: false,
-            fetch_url: "https://upki.rustls.dev/".into(),
+            fetch_url: "https://upki.rustls.dev/intermediates/".into(),
         }
     }
 }
